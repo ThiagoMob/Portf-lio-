9efe72de6eed539d474921db7c68f368369e8521
Thiago Xavier <thiagoxavier9966@gmail.com>
2022-02-16 17:47:28 -0300

    header provisoriamente pronto.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,12 @@
-const btnMobile = document.getElementById('btn-mobile')
+const introImg = document.querySelector('.intro-img img')
+    introImg.classList.add('active')
 
-function toggleMenu(event){
-    if(event.type == 'touchstart') event.preventDefault();
-    
-    const nav = document.getElementById('nav') //pegando a nav
-    nav.classList.toggle('active') //toggle significa, add caso não tenha, e remova caso não tenha
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded', active)
-    if(active){
-        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
-    }else{
-        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
-    }
-}
+const circle = document.querySelector('.circle')
+const container = document.querySelector('.container')
+const header = document.querySelector('.header')
+    circle.addEventListener('click', function(){
+        circle.classList.toggle('active')
+        container.classList.toggle('active')
+        header.classList.toggle('active')
 
-btnMobile.addEventListener('click', toggleMenu)
-btnMobile.addEventListener('touchstart', toggleMenu)
+    })
