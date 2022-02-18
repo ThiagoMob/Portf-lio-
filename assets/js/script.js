de2ad2010467ee018d2d44812af0e9a7a9c5707b
Thiago Xavier <thiagoxavier9966@gmail.com>
2022-02-18 18:02:24 -0300

    finalizado!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-const introImg = document.querySelector('.intro-img img')
-    introImg.classList.add('active')
+function active(){
+    const introImg = document.querySelector('.intro-img img')
+        introImg.classList.add('active')
 
-const circle = document.querySelector('.circle')
-const container = document.querySelector('.container')
-const header = document.querySelector('.header')
+    const circle = document.querySelector('.circle')
+    const container = document.querySelector('.container')
+    const header = document.querySelector('.header')
+
     circle.addEventListener('click', function(){
         circle.classList.toggle('active')
         container.classList.toggle('active')
         header.classList.toggle('active')
-
     })
+}
+    active()
